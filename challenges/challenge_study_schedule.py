f8a452591a7432de441bdd5416ed2ef222f2ceda
Lucas Matos <lucaslmatos18@gmail.com>
2023-08-18 15:26:34 -0300

    Req 01 - Implementado função study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,16 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+
+    if target_time == None or permanence_period == None:
+        return None
+
+    array_size = len(permanence_period)
+    count = 0
+
+    for index in range(array_size):
+        if None in permanence_period[index]: 
+            return None
+        if type(permanence_period[index][0]) != int or type(permanence_period[index][1]) != int:
+            return None
+        if permanence_period[index][1] >= target_time and permanence_period[index][0] <= target_time:
+            count += 1
+    return count
