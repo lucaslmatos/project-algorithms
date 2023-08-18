230b13708f2884a70d2eecf2a71840a6f3923c5d
Lucas Matos <lucaslmatos18@gmail.com>
2023-08-18 15:51:28 -0300

    Req 01 - Implementado função study_schedule (fluke8)

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,16 +1,18 @@
 def study_schedule(permanence_period, target_time):
 
-    if target_time == None or permanence_period == None:
+    if target_time is None or permanence_period is None:
         return None
 
     array_size = len(permanence_period)
     count = 0
 
     for index in range(array_size):
-        if None in permanence_period[index]: 
+        arg_one = permanence_period[index][0]
+        arg_two = permanence_period[index][1]
+        if (None in permanence_period[index] or
+                type(arg_one) != int or
+                type(arg_two) != int):
             return None
-        if type(permanence_period[index][0]) != int or type(permanence_period[index][1]) != int:
-            return None
-        if permanence_period[index][1] >= target_time and permanence_period[index][0] <= target_time:
+        if arg_two >= target_time and arg_one <= target_time:
             count += 1
     return count
