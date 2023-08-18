52910df13e264fb4c4ad12f045505f4aa3149875
Lucas Matos <lucaslmatos18@gmail.com>
2023-08-18 17:34:09 -0300

    Req 03 - Implementado função palindromes_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if high_index == -1:
+        return False
+    if word[low_index] != word[high_index]:
+        return False
+    if ((low_index == high_index) or (high_index - low_index) == 1):
+        return True
+    if word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
