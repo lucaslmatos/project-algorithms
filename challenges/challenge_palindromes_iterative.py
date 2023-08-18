7475d3f9959172602699085c536271e431afa32e
Lucas Matos <lucaslmatos18@gmail.com>
2023-08-18 18:03:45 -0300

    Req 06 - Implementado função palindromes_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,10 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    size = len(word)
+    palindrome = True
+    if size == 0:
+        return False
+    for index in range(size):
+        if word[index] != word[(size - 1) - index]:
+            palindrome = False
+            break
+    return palindrome
