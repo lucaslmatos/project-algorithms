5c49220c27d5711a890ddf28741f4a785f621edb
Lucas Matos <lucaslmatos18@gmail.com>
2023-08-18 16:30:15 -0300

    Req 02 - Implementado testes para função encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    "Se key não possuir o tipo correto, lançar exceção"
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("oi", "oi")
+    "Se message não possuir o tipo correto, lançar exceção"
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(1, 3)
+    assert encrypt_message("umafrase", 9) == "esarfamu"
+    assert encrypt_message("umafase", 4) == "esa_famu"
+    assert encrypt_message("umafas", 3) == "amu_saf"
