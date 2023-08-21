6381f534d19dc88752258557640236e8805ad062
Lucas Matos <lucaslmatos18@gmail.com>
2023-08-21 16:01:09 -0300

    Req 05 - Implementado função find_the_duplicate

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,6 +1,6 @@
 def is_anagram(first_string, second_string):
-    first_string_sorted = custom_sort(first_string.lower())
-    second_string_sorted = custom_sort(second_string.lower())
+    first_string_sorted = custom_sort_quick(first_string.lower())
+    second_string_sorted = custom_sort_quick(second_string.lower())
     if len(first_string) == 0 or len(second_string) == 0:
         return (first_string_sorted, second_string_sorted, False)
     if (first_string_sorted) == (second_string_sorted):
@@ -8,7 +8,7 @@ def is_anagram(first_string, second_string):
     return (first_string_sorted, second_string_sorted, False)
 
 
-def custom_sort(input_string):
+def custom_sort_quick(input_string):
 
     char_list = list(input_string)
     length = len(char_list)
@@ -17,19 +17,19 @@ def custom_sort(input_string):
     return sorted_string
 
 
-def partition(arr, low, high):
-    pivot = arr[high]
-    i = low - 1
-    for j in range(low, high):
-        if arr[j] <= pivot:
+def partition(list, low_index, high_index):
+    pivot = list[high_index]
+    i = low_index - 1
+    for j in range(low_index, high_index):
+        if list[j] <= pivot:
             i += 1
-            arr[i], arr[j] = arr[j], arr[i]
-    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+            list[i], list[j] = list[j], list[i]
+    list[i + 1], list[high_index] = list[high_index], list[i + 1]
     return i + 1
 
 
-def quicksort(arr, low, high):
-    if low < high:
-        pi = partition(arr, low, high)
-        quicksort(arr, low, pi - 1)
-        quicksort(arr, pi + 1, high)
+def quicksort(list, low_index, high_index):
+    if low_index < high_index:
+        pi = partition(list, low_index, high_index)
+        quicksort(list, low_index, pi - 1)
+        quicksort(list, pi + 1, high_index)
--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,11 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(nums) <= 1 or type(nums) == str:
+        return False
+    num_count = {}
+    for num in nums:
+        if type(num) == str or num < 0:
+            return False
+        if num in num_count:
+            return num
+        num_count[num] = 1
+    return False
